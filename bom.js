e59acf55e4ebc5ed34dd698174db4325305c1771
Rahul-Raj-R <rahulrajan9384@gmail.com>
2025-08-25 11:14:03 +0530

    Add files via upload

---
+++ bom.js
@@ -0,0 +1,9 @@
+
+alert("This is an alert message!");
+
+let isConfirmed = confirm("Do you want to continue?");
+if (isConfirmed) {
+  alert("You chose OK");
+} else {
+  alert("You chose Cancel");
+}
